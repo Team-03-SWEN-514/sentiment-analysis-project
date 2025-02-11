518fae03595a934ecea617c30d1aa3047c7d52e1
TChoy04 <tszfai04@gmail.com>
2025-02-11 15:23:41 -0500

    terraform

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+ec2_instance_type = "t2.micro"
+instance_name = "Group 3 Terraform Project"
+key_pair = "MY_AWS_KEY"
---
+++ terraform/variables.tf
@@ -0,0 +1,32 @@
+variable "instance_name"{
+    description = "Value of the Name tag for EC2 instance"
+    type = string
+    default = "My TF Instance"
+}
+variable "ec2_instance_type"{
+    description = "My EC2 instance type"
+    type = string
+    default = "t2.micro"
+}
+variable "ami_id"{
+    description = "AMI ID"
+    type = string
+    default = "ami-05ecb09ab7b9e1f90"
+}
+variable "key_pair"{
+    description = "My keypar"
+    type = string
+    default = "default-keypair"
+}
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.0.0/26", "10.0.0.64/26"]
+
+}
+
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.0.128/26", "10.0.0.192/26"]
+}
