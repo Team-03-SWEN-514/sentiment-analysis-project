9d5e54047dab3e6763cb1588e9b12ef1da26827d
xokem <ex010@live.com>
2025-03-27 15:14:16 -0400

    Fix tailwind setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./app.css";
 
 import { HeroUIProvider } from "@heroui/react";
+import '@/app/globals.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     // ...
     // make sure it's pointing to the ROOT node_module
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+	"./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
