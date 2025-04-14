f280c61a0deeeb102d0483bb6eb7e5cd3095c3c8
Gloobinours <kusami.louan@gmail.com>
2025-04-14 17:48:49 -0400

    removed cloudlogs.tf for PR

--- terraform/cloudlogs.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_cloudwatch_log_group" "stock_log_group" {
-  name = "stock-log-group"
-}
-
-resource "aws_cloudwatch_log_stream" "example_log_stream" {
-  name = "stock-log-stream"
-  log_group_name = aws_cloudwatch_log_group.stock_log_group.name
-}
