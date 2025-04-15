202976fd7501bf39a503faa785a32b0c92fb89a5
MaeveA2025 <eja9979@rit.edu>
2025-04-15 16:10:34 -0400

    added cors

--- terraform/cors_apigateway.tf
+++ terraform/cors_apigateway.tf
@@ -134,41 +134,6 @@ resource "aws_api_gateway_integration_response" "options_sns_response" {
   }
 }
 
-
-resource "aws_api_gateway_integration_response" "sns_publish_response" {
-  rest_api_id = aws_api_gateway_rest_api.market_api.id
-  resource_id = aws_api_gateway_resource.sns_publish.id
-  http_method = aws_api_gateway_method.publish_message.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS,PATCH'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
-
-resource "aws_api_gateway_method_response" "publish_message_response" {
-  rest_api_id = aws_api_gateway_rest_api.market_api.id
-  resource_id = aws_api_gateway_resource.sns_publish.id
-  http_method = aws_api_gateway_method.publish_message.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-  
-  response_templates = {
-    "application/json" = ""
-  }
-}
-
 resource "aws_api_gateway_method_response" "options_sns" {
   rest_api_id = aws_api_gateway_rest_api.market_api.id
   resource_id = aws_api_gateway_resource.sns.id
@@ -179,6 +144,43 @@ resource "aws_api_gateway_method_response" "options_sns" {
     "method.response.header.Access-Control-Allow-Headers" = true
     "method.response.header.Access-Control-Allow-Methods" = true
     "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_method" "options_sns_publish" {
+  rest_api_id   = aws_api_gateway_rest_api.market_api.id
+  resource_id   = aws_api_gateway_resource.sns_publish.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options_sns_publish" {
+  rest_api_id          = aws_api_gateway_rest_api.market_api.id
+  resource_id          = aws_api_gateway_resource.sns_publish.id
+  http_method          = aws_api_gateway_method.options_sns_publish.http_method
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_MATCH"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_sns_publish_response" {
+  depends_on  = [aws_api_gateway_integration.options_sns_publish]
+  rest_api_id = aws_api_gateway_rest_api.market_api.id
+  resource_id = aws_api_gateway_resource.sns_publish.id
+  http_method = aws_api_gateway_method.options_sns_publish.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS,PATCH'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
   }
 }
 
--- terraform/sns_apigateway.tf
+++ terraform/sns_apigateway.tf
@@ -12,12 +12,6 @@ resource "aws_api_gateway_method" "subscribe_email" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "publish_message" {
-  rest_api_id   = aws_api_gateway_rest_api.market_api.id
-  resource_id   = aws_api_gateway_resource.sns.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
 
 resource "aws_api_gateway_integration" "sns_proxy" {
   rest_api_id             = aws_api_gateway_rest_api.market_api.id
@@ -28,15 +22,6 @@ resource "aws_api_gateway_integration" "sns_proxy" {
   uri                     = aws_lambda_function.sns_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_integration" "sns_publish_proxy" {
-  rest_api_id             = aws_api_gateway_rest_api.market_api.id
-  resource_id             = aws_api_gateway_resource.sns_publish.id
-  http_method             = aws_api_gateway_method.publish_message.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.sns_publish_lambda.invoke_arn
-}
-
 # Permission for POST -> sns_event_function
 resource "aws_lambda_permission" "allow_apigw_invoke_sns" {
   statement_id  = "AllowAPIGatewayInvokePOST"
@@ -44,4 +29,27 @@ resource "aws_lambda_permission" "allow_apigw_invoke_sns" {
   function_name = aws_lambda_function.sns_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.market_api.execution_arn}/*/POST/sns"
+}
+
+# /sns/publish
+resource "aws_api_gateway_resource" "sns_publish" {
+  rest_api_id = aws_api_gateway_rest_api.market_api.id
+  parent_id   = aws_api_gateway_resource.sns.id
+  path_part   = "publish"
+}
+
+resource "aws_api_gateway_method" "publish_message" {
+  rest_api_id   = aws_api_gateway_rest_api.market_api.id
+  resource_id   = aws_api_gateway_resource.sns_publish.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "sns_publish_proxy" {
+  rest_api_id             = aws_api_gateway_rest_api.market_api.id
+  resource_id             = aws_api_gateway_resource.sns_publish.id
+  http_method             = aws_api_gateway_method.publish_message.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.sns_publish_lambda.invoke_arn
 }
